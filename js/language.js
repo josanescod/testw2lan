ac65cb6cce24f919fea246aa78fa12b6852a6fb7
josan <josanescod@gmail.com>
2020-09-16 20:46:40 +0200

    refactorizacion codigo 3

--- js/language.js
+++ js/language.js
@@ -1,26 +1,28 @@
 /*codigo para cambiar el idioma de castellano a catalan, por defecto en castellano*/
 var seleccion = 'es';
+var file = 'es.json'
 window.onload = () => {
-    cargaEs()
+    cargaLenguaje(file);
     seleccionLengua();
 }
 
 function seleccionLengua() {
     let selectLanguage = document.querySelector('#idioma')
     selectLanguage.addEventListener('change', function () {
-        let valorIdioma = this.options[this.selectedIndex].value;
-        seleccion = valorIdioma;
+        seleccion = this.options[this.selectedIndex].value;
         if (seleccion === 'es') {
-            cargaEs();
+            file = 'es.json'
+            cargaLenguaje(file)
         } else {
-            cargaCat();
-            ;
+            file = 'cat.json'
+            cargaLenguaje(file);
         }
     });
 }
 
-function cargaEs() {
-    fetch('./language/es.json')
+function cargaLenguaje(file) {
+      
+    fetch(`./language/${file}`)
         .then(
             function (response) {
                 if (response.status !== 200) {
@@ -32,7 +34,7 @@ function cargaEs() {
                 // Examine the text in the response
                 response.json().then(function (data) {
                     console.log(data);
-                    muestraDatos(data)
+                    muestraDatos(data);
                 });
             }
         )
@@ -55,44 +57,5 @@ function cargaEs() {
 
 }
 
-function cargaCat() {
-    fetch('./language/cat.json')
-        .then(
-            function (response) {
-                if (response.status !== 200) {
-                    console.log('Ha habido algun problema. Status Code: ' +
-                        response.status);
-                    return;
-                }
-
-                // Examine the text in the response
-                response.json().then(function (data) {
-                    console.log(data);
-                    mostraDades(data);
-                });
-            }
-        )
-        .catch(function (err) {
-            console.log('Fetch Error :-S', err);
-        });
-
-    function mostraDades(data) {
-        let eleccion = document.querySelector('#resultado')
-        let titulo = document.querySelector('#titulo')
-        let p1 = document.querySelector('#p1')
-        let p2 = document.querySelector('#p2')
-        // array de elementos
-        let elementos = [eleccion,titulo,p1,p2];
-        let datos = [data['eleccio'],data['titol'],data['comentari1'],data['comentari2']]
-        for (i=0;i < elementos.length;i++){
-            elementos[i].innerHTML=datos[i];
-        }
-    }
-
-
-}
-
-
 //mejoras
-//https://stackoverflow.com/questions/50983150/how-to-pass-a-variable-with-url-on-javascript-fetch-method --> pasar variable a fetch?
 //https://stackoverflow.com/questions/37673454/javascript-iterate-key-value-from-json --> recorrer un objeto json
